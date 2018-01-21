616d49630fe3cb0675ddc379996ce06ff67110a3
Dmitry Porokhnya <dmporokhnya@gmail.com>
2018-01-21 14:43:43 +0200

    Added sevice, DI container

--- modules/custom/hello_world/src/Controller/HelloWorldController.php
+++ modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -10,12 +10,34 @@ namespace Drupal\hello_world\Controller;
 
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\hello_world\HelloWorldSalutation;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class HelloWorldController extends ControllerBase {
 
+  /**
+   * @var \Drupal\hello_world\HelloWorldSalutation
+   */
+  protected $salutation;
+
+  /**
+   * HelloWorldController constructor.
+   *
+   * @param \Drupal\hello_world\HelloWorldSalutation $salutation
+   */
+  public function __construct(HelloWorldSalutation $salutation) {
+    $this->salutation = $salutation;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('hello_world.salutation')
+    );
+  }
+
   function HelloWorld() {
     return [
-      '#markup' => $this->t('Hell in a world'),
+      '#markup' => $this->salutation->getSalutation(),
     ];
   }
 }
