d0924dc2bc138340d185dda8391b955370dd2cda
Dmitry Porokhnya <dmporokhnya@gmail.com>
2018-01-21 10:12:48 +0200

    Added service class

---
+++ modules/custom/hello_world/src/HelloWorldSalutation.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Дмитрий
+ * Date: 21.01.2018
+ * Time: 09:56
+ */
+
+namespace Drupal\hello_world;
+
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Prepares the salutation to the world.
+ */
+class HelloWorldSalutation {
+
+  use StringTranslationTrait;
+
+  /**
+   * Returns the salutation
+   */
+  public function getSalutation() {
+    $time = new \DateTime();
+    $g_time = (int) $time->format('G');
+    if ($g_time >= 06 && $g_time < 12) {
+      return $this->t('Good morning world');
+    }
+
+    if ($g_time >= 12 && $g_time < 18) {
+      return $this->t('Good afternoon world');
+    }
+
+    if ($g_time >= 18) {
+      return $this->t('Good evening world');
+    }
+  }
+
+}
